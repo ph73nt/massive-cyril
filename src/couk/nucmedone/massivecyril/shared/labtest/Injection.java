4f2431ab0f5d31387778da06b80e5aa79e668878
Neil Thomson <neil@pukka-j.com>
2013-09-11 10:41:26 +0100

    Added GPL and headers

--- src/couk/nucmedone/massivecyril/shared/labtest/Injection.java
+++ src/couk/nucmedone/massivecyril/shared/labtest/Injection.java
@@ -1,3 +1,23 @@
+/**
+ * @author Neil J Thomson (njt@fishlegs.co.uk)
+ *
+ * Copyright (C) 2013 Neil J Thomson
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
 package couk.nucmedone.massivecyril.shared.labtest;
 
 import java.util.Calendar;
@@ -6,7 +26,7 @@ import java.util.Calendar;
  * A class to hold parameters for nuclear counting test administrations such as
  * a syringe of Cr-51 EDTA.
  * 
- * @author neil@pukka-j.com
+ * @author Neil J Thomson
  * 
  */
 public class Injection {
--- src/couk/nucmedone/massivecyril/shared/labtest/WatsonSingleSampleGFR.java
+++ src/couk/nucmedone/massivecyril/shared/labtest/WatsonSingleSampleGFR.java
@@ -1,3 +1,23 @@
+/**
+ * @author Neil J Thomson (njt@fishlegs.co.uk)
+ *
+ * Copyright (C) 2013 Neil J Thomson
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
 package couk.nucmedone.massivecyril.shared.labtest;
 
 import couk.nucmedone.massivecyril.shared.labtest.exceptions.TimeTooShortFromAdminException;
@@ -7,7 +27,7 @@ import couk.nucmedone.massivecyril.shared.labtest.exceptions.TimeTooShortFromAdm
  * plasma sample. <br /><br />Reference:<br />A simple method of estimating glomerular filtration
  * rate, WS Watson, Eur. J. Nucl. Med. 1992(19):827
  * 
- * @author neil
+ * @author Neil J Thomson
  * 
  */
 public class WatsonSingleSampleGFR extends GFR {
